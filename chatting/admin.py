ea72094a69fa56abcb1590258e61ab92ce75153d
mohammed <mnmn030405@gmail.com>
2023-12-23 20:48:01 +0300

    fixing buges for the chatting and pages from admin site

--- chatting/admin.py
+++ chatting/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from .models import Chat, Order, Booking, Area, City, Property, Type, Report_Agent
+from .models import Chat, Report_Agent
 
 # Register your models here.
 admin.site.register(Chat)
-admin.site.register(Order)
-admin.site.register(Booking)
-admin.site.register(Area)
-admin.site.register(City)
-admin.site.register(Property)
-admin.site.register(Type)
 admin.site.register(Report_Agent)
--- pages/admin.py
+++ pages/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from .models import User, Notification, Financial_Movement, Comparison, Photo_Property, Favorite, ADS, Photo_ADS, Evaluation, Report_Customer
 
 # Register your models here.
-
-admin.site.register(User)
-admin.site.register(Notification)
-admin.site.register(Financial_Movement)
-admin.site.register(Comparison)
-admin.site.register(Photo_Property)
-admin.site.register(Favorite)
-admin.site.register(ADS)
-admin.site.register(Photo_ADS)
-admin.site.register(Evaluation)
-admin.site.register(Report_Customer)
