9b98d7b39efedd43d1df62b347a6527155bcca2b
ebtesam <ebtesamyaha346@gamil.com>
2023-12-25 12:12:59 +0300

    link Property.model with other classes

--- property/models.py
+++ property/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from accounts.models import Agent,Customer
+from .models import Type,Area,Property
 
 # Create your models here.
 class Property(models.Model):
-    type_id = models.IntegerField()
-    area_id = models.IntegerField()
-    user_ID = models.IntegerField()
+    type = models.ForeignKey(Type, on_delete=models.DO_NOTHING)
+    area = models.IntegerField(Area, on_delete=models.DO_NOTHING)
+    agent = models.IntegerField(Agent, on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=50)
     tescription = models.CharField(max_length=100)
     features = models.CharField(max_length=100)
@@ -14,7 +15,7 @@ class Property(models.Model):
 
 
 class Photo_Property(models.Model):
-    property_id = models.IntegerField()
+    property = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
     description = models.CharField(max_length=100)
     path = models.CharField(max_length=100)
 
@@ -24,21 +25,21 @@ class Type(models.Model):
 
 
 class Comparison(models.Model):
-    property_id = models.IntegerField()
+    property = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     location = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
 
 
 class Favorite(models.Model):
-    property_id = models.IntegerField()
-    user_id = models.IntegerField()
+    property = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
+    customer = models.OneToOneField(Customer,on_delete=models.DO_NOTHING)
     status = models.CharField(max_length=100)
 
 
 class ADS(models.Model):
-    property_id = models.IntegerField()
-    user_id = models.IntegerField()
+    property = models.ForeignKey(Property, on_delete=models.DO_NOTHING)
+    agent = models. ForeignKey(Agent,on_delete=models.DO_NOTHING)
 
 
 class Photo_ADS(models.Model):
