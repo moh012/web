54abbde3dd9183300d93538d27349b8e9c2e54ca
mohammed <mnmn030405@gmail.com>
2024-01-15 12:25:13 +0300

    حذف التمبلت السابق واضافة التمبلت الجديد الخاص بالعقارات

--- pages/views.py
+++ pages/views.py
@@ -7,14 +7,49 @@ def index(request):
     return render(request, 'pages/index.html')
 
 
-def createAccount(request):
-    return render(request, 'pages/createAccount.html')
+def about(request):
+    return render(request, 'pages/about.html')
 
-def logIn(request):
-    return render(request, 'pages/logIn.html')
 
-def phoneNumber(request):
-    return render(request, 'pages/phoneNumber.html')
+def agent_single(request):
+    return render(request, 'pages/agent-single.html')
 
-def verificationCode(request):
-    return render(request, 'pages/verificationCode.html')
+
+def agents_grid(request):
+    return render(request, 'pages/agents-grid.html')
+
+
+def blog_grid(request):
+    return render(request, 'pages/blog-grid.html')
+
+
+def blog_single(request):
+    return render(request, 'pages/blog-single.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def login(request):
+    return render(request, 'pages/login.html')
+
+
+def order_grid(request):
+    return render(request, 'pages/order-grid.html')
+
+
+def order_single(request):
+    return render(request, 'pages/order-single.html')
+
+
+def property_grid(request):
+    return render(request, 'pages/property-grid.html')
+
+
+def property_single(request):
+    return render(request, 'pages/property-single.html')
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,237 +1,475 @@
-
-(function ($) {
+/**
+* Template Name: EstateAgency
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
   "use strict";
 
-  
-  /*==================================================================
-  [ Validate ]*/
-  var input = $('.validate-inputA .input100A');
-
-  $('.validate-formA').on('submit',function(){
-      var check = true;
-
-      for(var i=0; i<input.length; i++) {
-          if(validate(input[i]) == false){
-              showValidate(input[i]);
-              check=false;
-          }
-      }
-
-      return check;
-  });
-
-
-  
-
-  $('.validate-formA .input100A').each(function(){
-      $(this).focus(function(){
-         hideValidate(this);
-      });
-  });
-
-  function validate (input) {
-      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-              return false;
-          }
-      }
-      else {
-          if($(input).val().trim() == ''){
-              return false;
-          }
-      }
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
   }
 
-  function showValidate(input) {
-      var thisAlert = $(input).parent();
-
-      $(thisAlert).addClass('alert-validate');
-  }
-
-  function hideValidate(input) {
-      var thisAlert = $(input).parent();
-
-      $(thisAlert).removeClass('alert-validate');
-  }
-  
-  
-  
-
-})(jQuery);
-
-
-
-
-
-
-
-
-
-// page 2
-
-document.addEventListener('input', function (e) {
-  var input = e.target;
-  if (input.tagName.toLowerCase() === 'input' && input.type === 'text') {
-    var maxLength = parseInt(input.getAttribute('maxlength'), 10);
-    var currentLength = input.value.length;
-
-    if (currentLength === maxLength) {
-      var nextInput = input.nextElementSibling;
-      if (nextInput) {
-        nextInput.focus();
-      }
-    } else if (currentLength === 0) {
-      var previousInput = input.previousElementSibling;
-      if (previousInput) {
-        previousInput.focus();
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
       }
     }
   }
-});
 
-document.addEventListener('keydown', function (e) {
-  var input = e.target;
-  if (e.key === 'Backspace' && input.tagName.toLowerCase() === 'input' && input.type === 'text') {
-    var currentLength = input.value.length;
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
 
-    if (currentLength === 0) {
-      var previousInput = input.previousElementSibling;
-      if (previousInput) {
-        previousInput.focus();
+  /**
+   * Toggle .navbar-reduce
+   */
+  let selectHNavbar = select('.navbar-default')
+  if (selectHNavbar) {
+    onscroll(document, () => {
+      if (window.scrollY > 100) {
+        selectHNavbar.classList.add('navbar-reduce')
+        selectHNavbar.classList.remove('navbar-trans')
+      } else {
+        selectHNavbar.classList.remove('navbar-reduce')
+        selectHNavbar.classList.add('navbar-trans')
+      }
+    })
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
       }
     }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
   }
-});
 
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
 
+  /**
+   * Search window open/close
+   */
+  let body = select('body');
+  on('click', '.navbar-toggle-box', function(e) {
+    e.preventDefault()
+    body.classList.add('box-collapse-open')
+    body.classList.remove('box-collapse-closed')
+  })
 
-// page3
+  on('click', '.close-box-collapse', function(e) {
+    e.preventDefault()
+    body.classList.remove('box-collapse-open')
+    body.classList.add('box-collapse-closed')
+  })
 
-(function ($) {
-  "use strict";
-
-  
-  /*==================================================================
-  [ Validate ]*/
-  var input = $('.validate-input .input100');
-
-  $('.validate-form').on('submit',function(){
-      var check = true;
-
-      for(var i=0; i<input.length; i++) {
-          if(validate(input[i]) == false){
-              showValidate(input[i]);
-              check=false;
-          }
-      }
-
-      return check;
+  /**
+   * Intro Carousel
+   */
+  new Swiper('.intro-carousel', {
+    speed: 900,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
   });
 
+  /**
+   * Property carousel
+   */
+  new Swiper('#property-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.propery-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
 
-  $('.validate-form .input100').each(function(){
-      $(this).focus(function(){
-         hideValidate(this);
-      });
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+  
+
+  /**
+   * News carousel
+   */
+  new Swiper('#news-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.news-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
   });
 
-  function validate (input) {
-      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-              return false;
-          }
+  /**
+   * Property carousel
+   */
+  new Swiper('#property-carousel1', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.propery-carousel-pagination1',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
       }
-      else {
-          if($(input).val().trim() == ''){
-              return false;
-          }
-      }
-  }
-
-  function showValidate(input) {
-      var thisAlert = $(input).parent();
-
-      $(thisAlert).addClass('alert-validate');
-  }
-
-  function hideValidate(input) {
-      var thisAlert = $(input).parent();
-
-      $(thisAlert).removeClass('alert-validate');
-  }
-  
-  
-
-})(jQuery);
-
-
-
-
-
-
-// page4
-
-(function ($) {
-  "use strict";
-
-  
-  /*==================================================================
-  [ Validate ]*/
-  var input = $('.validate-input .input100');
-
-  $('.validate-form').on('submit',function(){
-      var check = true;
-
-      for(var i=0; i<input.length; i++) {
-          if(validate(input[i]) == false){
-              showValidate(input[i]);
-              check=false;
-          }
-      }
-
-      return check;
+    }
   });
 
-
-  $('.validate-form .input100').each(function(){
-      $(this).focus(function(){
-         hideValidate(this);
-      });
+  /**
+   * Testimonial carousel
+   */
+  new Swiper('#testimonial-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.testimonial-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    }
   });
 
-  function validate (input) {
-      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-              return false;
-          }
-      }
-      else {
-          if($(input).val().trim() == ''){
-              return false;
-          }
-      }
-  }
+  /**
+   * Property Single carousel
+   */
+  new Swiper('#property-single-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.property-single-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
 
-  function showValidate(input) {
-      var thisAlert = $(input).parent();
+})()
 
-      $(thisAlert).addClass('alert-validate');
-  }
+// start login pages js
 
-  function hideValidate(input) {
-      var thisAlert = $(input).parent();
+// (function ($) {
+//   "use strict";
 
-      $(thisAlert).removeClass('alert-validate');
-  }
+  
+//   /*==================================================================
+//   [ Validate ]*/
+//   var input = $('.validate-inputA .input100A');
+
+//   $('.validate-formA').on('submit',function(){
+//       var check = true;
+
+//       for(var i=0; i<input.length; i++) {
+//           if(validate(input[i]) == false){
+//               showValidate(input[i]);
+//               check=false;
+//           }
+//       }
+
+//       return check;
+//   });
+
+
+  
+
+//   $('.validate-formA .input100A').each(function(){
+//       $(this).focus(function(){
+//          hideValidate(this);
+//       });
+//   });
+
+//   function validate (input) {
+//       if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//           if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//               return false;
+//           }
+//       }
+//       else {
+//           if($(input).val().trim() == ''){
+//               return false;
+//           }
+//       }
+//   }
+
+//   function showValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).addClass('alert-validate');
+//   }
+
+//   function hideValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).removeClass('alert-validate');
+//   }
+  
+  
+  
+
+// })(jQuery);
+
+
+
+
+
+
+
+
+
+// // page 2
+
+// document.addEventListener('input', function (e) {
+//   var input = e.target;
+//   if (input.tagName.toLowerCase() === 'input' && input.type === 'text') {
+//     var maxLength = parseInt(input.getAttribute('maxlength'), 10);
+//     var currentLength = input.value.length;
+
+//     if (currentLength === maxLength) {
+//       var nextInput = input.nextElementSibling;
+//       if (nextInput) {
+//         nextInput.focus();
+//       }
+//     } else if (currentLength === 0) {
+//       var previousInput = input.previousElementSibling;
+//       if (previousInput) {
+//         previousInput.focus();
+//       }
+//     }
+//   }
+// });
+
+// document.addEventListener('keydown', function (e) {
+//   var input = e.target;
+//   if (e.key === 'Backspace' && input.tagName.toLowerCase() === 'input' && input.type === 'text') {
+//     var currentLength = input.value.length;
+
+//     if (currentLength === 0) {
+//       var previousInput = input.previousElementSibling;
+//       if (previousInput) {
+//         previousInput.focus();
+//       }
+//     }
+//   }
+// });
+
+
+
+// // page3
+
+// (function ($) {
+//   "use strict";
+
+  
+//   /*==================================================================
+//   [ Validate ]*/
+//   var input = $('.validate-input .input100');
+
+//   $('.validate-form').on('submit',function(){
+//       var check = true;
+
+//       for(var i=0; i<input.length; i++) {
+//           if(validate(input[i]) == false){
+//               showValidate(input[i]);
+//               check=false;
+//           }
+//       }
+
+//       return check;
+//   });
+
+
+//   $('.validate-form .input100').each(function(){
+//       $(this).focus(function(){
+//          hideValidate(this);
+//       });
+//   });
+
+//   function validate (input) {
+//       if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//           if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//               return false;
+//           }
+//       }
+//       else {
+//           if($(input).val().trim() == ''){
+//               return false;
+//           }
+//       }
+//   }
+
+//   function showValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).addClass('alert-validate');
+//   }
+
+//   function hideValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).removeClass('alert-validate');
+//   }
   
   
 
-})(jQuery);
+// })(jQuery);
 
 
 
 
 
 
+// // page4
+
+// (function ($) {
+//   "use strict";
+
+  
+//   /*==================================================================
+//   [ Validate ]*/
+//   var input = $('.validate-input .input100');
+
+//   $('.validate-form').on('submit',function(){
+//       var check = true;
+
+//       for(var i=0; i<input.length; i++) {
+//           if(validate(input[i]) == false){
+//               showValidate(input[i]);
+//               check=false;
+//           }
+//       }
+
+//       return check;
+//   });
 
 
+//   $('.validate-form .input100').each(function(){
+//       $(this).focus(function(){
+//          hideValidate(this);
+//       });
+//   });
+
+//   function validate (input) {
+//       if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+//           if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+//               return false;
+//           }
+//       }
+//       else {
+//           if($(input).val().trim() == ''){
+//               return false;
+//           }
+//       }
+//   }
+
+//   function showValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).addClass('alert-validate');
+//   }
+
+//   function hideValidate(input) {
+//       var thisAlert = $(input).parent();
+
+//       $(thisAlert).removeClass('alert-validate');
+//   }
+  
+  
+
+// })(jQuery);
+// end login pages js
