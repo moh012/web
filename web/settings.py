52139215259b3f2995d9c66339727525af924b14
Ethar <Parkh2045@gmail.com>
2024-01-21 13:07:11 +0300

    Ebtesam&Ethar have added cities photos

--- web/settings.py
+++ web/settings.py
@@ -129,3 +129,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'web/static')]
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/Media/'
