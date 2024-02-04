e32b29c3a33d85b8596596281de6ff40e6f21e26
mohammed <mnmn030405@gmail.com>
2024-02-05 01:17:56 +0300

    اضافة صفحة سياسة الخصوصية وشروط الاحكام وربطها مع الصفحات المخصصه بها

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('agents_grid', views.agents_grid, name='agents_grid'),
     path('agent_single', views.agent_single, name='agent_single'),
     path('login', views.login, name='login'),
+    path('privacy_policy', views.privacy_policy, name='privacy_policy'),
     path('signup', views.signup, name='signup'),
     path('userdata', views.userdata, name='userdata'),
     path('verification', views.verification, name='verification'),
