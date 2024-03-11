83550660bfc4e5ec129c4211dca5dbea6850d1ef
mohammed <mnmn030405@gmail.com>
2024-03-11 12:03:09 +0300

    ربط صفحة اضافة عقار مع قاعدة البيانات

--- property/models.py
+++ property/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from accounts.models import Agent, Customer
-
+from django.utils import timezone
 
 # Create your models here.
-class Type(models.Model):
-    search_type = models.CharField(max_length=100, null=True)
+# class Type(models.Model):
+#     search_type = models.CharField(max_length=100, null=True)
 
-    def __str__(self):
-        return self.type_name
+#     def __str__(self):
+#         return self.type_name
 
 
 class City(models.Model):
@@ -27,31 +27,34 @@ class Area(models.Model):
 
 
 class Property(models.Model):
-    type = models.ForeignKey(Type, on_delete=models.DO_NOTHING, null=True)
+    # type = models.ForeignKey(Type, on_delete=models.DO_NOTHING, null=True)
     area = models.ForeignKey(Area, on_delete=models.DO_NOTHING, null=True)
     agent = models.ForeignKey(Agent, on_delete=models.DO_NOTHING, null=True)
-    type_name = models.CharField(max_length=100, null=True)
-    property_type = models.CharField(max_length=100, null=True)
     title = models.CharField(max_length=50, null=True)
-    description = models.CharField(max_length=100, null=True)
-    features = models.CharField(max_length=100, null=True)
+    property_type = models.CharField(max_length=100, null=True)
     location = models.CharField(max_length=100, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    property_date = models.DateField(null=True)
-    state = models.CharField(max_length=100, null=True)
-    room_number = models.IntegerField(null=True)
-    floor = models.CharField(max_length=100, null=True)
+    img = models.ImageField(upload_to='propertyPhoto/%Y/%m/%d/', blank=True)
+    description = models.CharField(max_length=100, null=True)
     details = models.TextField(null=True)
-    build_year = models.CharField(max_length=100, null=True)
-    bathrooms = models.CharField(max_length=100, null=True)
+    price = models.IntegerField(null=True)
+    room_number = models.IntegerField(null=True)
     hall_room = models.CharField(max_length=100, null=True)
-    basement = models.CharField(max_length=100, null=True)
-    pool = models.CharField(max_length=100, null=True)
-    furnished = models.CharField(max_length=100, null=True)
-    elevator = models.CharField(max_length=100, null=True)
+    bathrooms = models.CharField(max_length=100, null=True)
+    floor = models.CharField(max_length=100, null=True)
     street_number = models.CharField(max_length=100, null=True)
-    appendix = models.CharField(max_length=100, null=True)
+    build_year = models.CharField(max_length=100, null=True)
+    house_type = models.CharField(max_length=100, null=True)
     space = models.IntegerField(null=True)
+    pool = models.BooleanField(default=False, null=True)
+    kitchen = models.BooleanField(default=False, null=True)
+    roof = models.BooleanField(default=False, null=True)
+    modern = models.BooleanField(default=False, null=True)
+    appendix = models.BooleanField(default=False, null=True)
+    elevator = models.BooleanField(default=False, null=True)
+    basement = models.BooleanField(default=False, null=True)
+    furnished = models.BooleanField(default=False, null=True)
+    property_date = models.DateField(default=timezone.now)
+    state = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.title
