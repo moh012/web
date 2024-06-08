071e39238b040b813113beb320a09779749e6ccb
Alwi <alwiProgrammer@gmail.com>
2024-06-08 03:02:22 +0300

    اصلاح خدمات نسيت كلمة السر

--- accounts/urls.py
+++ accounts/urls.py
@@ -21,4 +21,10 @@ urlpatterns = [
              template_name='accounts/password_change_done.html'),
          name='password_change_done'),
 
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset_form.html"), name='reset_password'),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password_reset_complete.html"), name='password_reset_complete'),
+
+
 ]
